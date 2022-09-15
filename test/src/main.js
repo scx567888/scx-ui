6d15bd33cac03b60745996288d7a02628ac569d5
scx567888 <scx567888@outlook.com>
2022-09-15 09:38:57 +0800

    dev

--- test/src/main.js
+++ test/src/main.js
@@ -5,12 +5,12 @@ import {ScxComponent} from "../../scx-component.js";
 import "../../_scx-theme/default.css";
 import "../../_scx-theme/dark.css";
 import 'scx-icon/register';
-import {ScxReqVueInstaller} from "../../_scx-req/ScxReqVueInstall.js";
-import {ScxFSSVueInstaller} from "../../_scx-fss/ScxFSSVueInstall.js";
+import {ScxReqVueInstaller} from "../../_scx-req/ScxReqVueInstaller.js";
+import {ScxFSSVueInstaller} from "../../_scx-fss/ScxFSSVueInstaller.js";
 import {ScxEventBus} from "../../scx-event-bus.js";
 import {ScxReq} from "../../scx-req.js";
 import {ScxFSS} from "../../scx-fss.js";
-import {ScxEventBusVueInstaller} from "../../_scx-event-bus/ScxEventBusVueInstall.js";
+import {ScxEventBusVueInstaller} from "../../_scx-event-bus/ScxEventBusVueInstaller.js";
 
 const scxEventBus = new ScxEventBus("http://127.0.0.1:8080");
 const scxReq = new ScxReq("http://127.0.0.1:8080");
