debfe0e20f7b7e8d749c5d24674701503db03c47
scx567888 <scx567888@outlook.com>
2022-10-13 17:17:21 +0800

    dev

--- test/src/views/scx-drag-test.vue
+++ test/src/views/scx-drag-test.vue
@@ -1,43 +1,127 @@
 <template>
   <div>
-    <div v-for="i in 100" v-drag="getDragEvent(i)" :style="{background:getColor()}" class="scx-drag-test-div">
-      {{ i }}
+    <label>
+      显示虚拟边界
+      <input v-model="showMargin" type="checkbox">
+    </label>
+
+    <div style="display: flex;flex-wrap: wrap">
+
+      <div v-for="i in 100" v-drag="getDragEvent2(i)" :style="{background:getColor()}" class="scx-drag-test-div2">
+        {{ i }}
+      </div>
+
+      <div ref="rightRef" class="scx-drag-test-div-wrapper right" :class="{showMargin}">
+        <div :style="{background:getColor()}" class="scx-drag-test-div1">
+          吸附右侧
+        </div>
+      </div>
+
+      <div ref="topRef" class="scx-drag-test-div-wrapper top" :class="{showMargin}">
+        <div :style="{background:getColor()}" class="scx-drag-test-div1">
+          吸附上侧
+        </div>
+      </div>
+
+      <div ref="leftRef" class="scx-drag-test-div-wrapper left" :class="{showMargin}">
+        <div :style="{background:getColor()}" class="scx-drag-test-div1">
+          吸附左侧
+        </div>
+      </div>
+
+      <div ref="bottomRef" class="scx-drag-test-div-wrapper bottom" :class="{showMargin}">
+        <div :style="{background:getColor()}" class="scx-drag-test-div1">
+          吸附下侧
+        </div>
+      </div>
+
     </div>
   </div>
-
 </template>
 
-<script>
-export default {
-  name: "scx-drag-test",
-  setup() {
+<script setup>
+import {onMounted, ref} from "vue";
+import {useScxDrag} from "../../../_scx-drag/index.js";
 
-    function getDragEvent(i) {
-      return {
-        onClick: (el) => {
-          console.log(i + " : onClick")
-        },
-        onDrag: (el) => {
-          console.log(i + " : onDrag")
-        },
-        onDragEnd: (el) => {
-          console.log(i + " : onDragEnd")
-        }
-      }
+
+const showMargin = ref(false);
+
+const rightRef = ref();
+const topRef = ref();
+const leftRef = ref();
+const bottomRef = ref();
+
+function getDragEvent2(i) {
+  return {
+    onClick: (el) => {
+      console.log(i + " : onClick")
+    },
+    onDrag: (el) => {
+      console.log(i + " : onDrag")
+    },
+    onDragEnd: (el) => {
+      console.log(i + " : onDragEnd")
     }
-
-    function getColor() {
-      return "#" + Math.random().toString(16).slice(-6);
-    }
-
-    return {getColor, getDragEvent}
   }
 }
+
+function getDragEvent(i) {
+  return {
+    onClick: (el) => {
+      console.log(i + " : onClick")
+    },
+    onDrag: (el) => {
+      el.classList.add("dragging")
+      console.log(i + " : onDrag")
+    },
+    onDragEnd: (el, startMatrix) => {
+      console.log(i + " : onDragEnd")
+      el.classList.remove("dragging")
+      let nowMatrix = new DOMMatrix(window.getComputedStyle(el).transform);
+      //清楚我们不需要的坐标
+      if (i === "top" || i === "bottom") {
+        nowMatrix = nowMatrix.translate(0, -nowMatrix.f)
+      } else if (i === "left" || i === "right") {
+        nowMatrix = nowMatrix.translate(-nowMatrix.e, 0)
+      }
+      el.style.transform = nowMatrix.toString()
+      el.classList.add("transform-none")
+    }
+  }
+}
+
+function getColor() {
+  return "#" + Math.random().toString(16).slice(-6);
+}
+
+onMounted(() => {
+
+  const getO = (i) => {
+    return {
+      callback: getDragEvent(i),
+      bounds: (a) => {
+        if (showMargin.value) {
+          a.bottom = a.bottom - 100
+          a.top = a.top + 100
+          a.left = a.left + 100
+          a.right = a.right - 100
+        }
+        return a;
+      }
+    }
+  }
+
+  useScxDrag(leftRef.value, getO("left"));
+  useScxDrag(topRef.value, getO("top"));
+  useScxDrag(rightRef.value, getO("right"));
+  useScxDrag(bottomRef.value, getO("bottom"));
+
+});
+
 </script>
 
 <style scoped>
-.scx-drag-test-div {
-  position: fixed;
+.scx-drag-test-div2 {
   width: 100px;
   height: 100px;
   display: flex;
@@ -47,4 +131,97 @@ export default {
   cursor: move;
   user-select: none;
 }
+
+.scx-drag-test-div-wrapper {
+  z-index: 1;
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  cursor: move;
+  user-select: none;
+  transition: all 100ms ease-in;
+}
+
+.scx-drag-test-div-wrapper.right {
+  right: 0px;
+  top: 50%;
+}
+
+.scx-drag-test-div-wrapper.bottom {
+  bottom: 0px;
+  left: 50%;
+}
+
+.scx-drag-test-div-wrapper.top {
+  top: 0px;
+  left: 50%;
+}
+
+.scx-drag-test-div-wrapper.left {
+  left: 0px;
+  top: 50%;
+}
+
+.transform-none {
+  transform: none;
+}
+
+.scx-drag-test-div1 {
+  z-index: 1;
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  cursor: move;
+  user-select: none;
+  transition: all 100ms ease-in;
+}
+
+.dragging {
+  transition: none;
+}
+
+@keyframes r {
+
+  0% {
+    transform: rotate(0deg) scale(1);
+  }
+
+  50% {
+    transform: rotate(180deg) scale(0.8);
+  }
+  100% {
+    transform: rotate(360deg) scale(1);
+  }
+
+}
+
+.dragging > .scx-drag-test-div1 {
+  border-radius: 10px;
+  animation: r 3s linear infinite;
+  background: darkmagenta !important;
+}
+
+.showMargin.right {
+  right: 100px;
+}
+
+.showMargin.top {
+  top: 100px;
+}
+
+.showMargin.left {
+  left: 100px;
+}
+
+.showMargin.bottom {
+  bottom: 100px;
+}
+
 </style>
