d9ecb809b9810b702da9383103376c69717dd402
scx567888 <scx567888@outlook.com>
2022-10-19 16:27:07 +0800

    dev

--- vanilla-download.js
+++ vanilla-download.js
@@ -1,23 +1,31 @@
 import {isString} from "./vanilla-type-helper.js";
+import {notNull} from "./vanilla-object-helper.js";
 
 /**
  * 根据 url 下载文件
  * @param url {string|Blob} 下载地址
  * @param fileName 下载文件的名称 为空取文件原始名
  */
-function download(url, fileName) {
+function download(url, fileName = null) {
     const link = document.createElement('a');
     link.rel = 'noopener';
     link.target = '_blank'
 
-    if (fileName) {
-        link.download = fileName;
-    }
+    link.download = notNull(fileName) ? fileName : '';
 
     if (isString(url)) { // 标准 URL
         link.href = url
         if (link.origin !== location.origin) {
-            fetch(link.href, {method: "GET"}).then(r => r.blob()).then(b => download(b, name)).catch(e => link.click());
+            fetch(link.href, {method: "GET"}).then(r => {
+                if (r.ok) {
+                    return r.blob();
+                } else {
+                    throw new Error(r.status + " : " + r.statusText);
+                }
+            }).then(b => download(b, fileName)).catch(e => {
+                console.error(e);
+                link.click();
+            });
         } else {
             link.click();
         }
