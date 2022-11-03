75020936b7cfc3495ac3128984db4d5a16e90a29
scx567888 <scx567888@outlook.com>
2022-11-03 14:48:46 +0800

    1.2.1

--- vanilla-download.js
+++ vanilla-download.js
@@ -1,5 +1,25 @@
 import {isString} from "./vanilla-type-helper.js";
-import {notNull} from "./vanilla-object-helper.js";
+import {isNull, notBlank, notNull} from "./vanilla-object-helper.js";
+
+function getFileNameFromContentDisposition(str) {
+    if (!str) {
+        return null;
+    }
+    const s = str.split(";");
+    //优先使用 filename* 请查看 : https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Headers/Content-Disposition
+    let fileNameElement = s.filter(c => c.startsWith("filename*="))[0];
+    if (!fileNameElement) {
+        fileNameElement = s.filter(c => c.startsWith("filename="))[0];
+    }
+    if (!fileNameElement) {// 两者都没有
+        return null;
+    }
+    const fileNameWithCharset = fileNameElement.split("=")[1];
+    const fs = fileNameWithCharset.split("''");
+    //这里还可以获取到 charset 但是用不到 所以就管了
+    const encodedFileName = fs[fs.length - 1];
+    return notBlank(encodedFileName) ? decodeURIComponent(encodedFileName) : null;
+}
 
 /**
  * 根据 url 下载文件
@@ -13,11 +33,17 @@ function download(url, fileName = null) {
 
     link.download = notNull(fileName) ? fileName : '';
 
-    if (isString(url)) { // 标准 URL
+    if (isString(url) || url instanceof URL) { // 标准 URL
         link.href = url
         if (link.origin !== location.origin) {
             fetch(link.href, {method: "GET"}).then(r => {
                 if (r.ok) {
+                    if (isNull(fileName)) { //如果原有的下载名称为空 我们尝试使用 content-disposition 中的文件名
+                        const f = getFileNameFromContentDisposition(r.headers.get("content-disposition"));
+                        if (notNull(f)) {
+                            fileName = f;
+                        }
+                    }
                     return r.blob();
                 } else {
                     throw new Error(r.status + " : " + r.statusText);
@@ -37,5 +63,6 @@ function download(url, fileName = null) {
 }
 
 export {
-    download
+    download,
+    getFileNameFromContentDisposition
 }
