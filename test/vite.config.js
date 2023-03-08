e6a0a18af094d32c431a17569c7f2ac5d4ca7c53
scx567888 <scx567888@outlook.com>
2023-03-08 14:44:49 +0800

    v1.2.6 (#28)

--- test/vite.config.js
+++ test/vite.config.js
@@ -1,14 +1,14 @@
-import vuePlugin from '@vitejs/plugin-vue';
-import {scxIconPlugin} from '../scx-icon-vite-plugins.js';
+import vue from '@vitejs/plugin-vue';
+import {scxIconPluginUseJS} from '../scx-icon-vite-plugins.js';
 
 export default {
     base: './',
-    plugins: [vuePlugin({
+    plugins: [vue({
         template: {
             compilerOptions: {
                 //scx-loading 并不是 vue 组件 所以在这里排除
                 isCustomElement: tag => ['scx-loading'].includes(tag)
             }
         }
-    }), scxIconPlugin({type: 'js', svgRoot: ['a']})]
+    }), scxIconPluginUseJS(['a'])]
 }
