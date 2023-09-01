cc535e908504bd3e59b7145a738f92d19c8531f0
scx567888 <scx567888@outlook.com>
2023-09-01 15:58:38 +0800

    v1.3.7 (#45)

--- vue-component/index.js
+++ vue-component/index.js
@@ -1,9 +1,12 @@
-export * from "./scx-component.js";
-export * from "./scx-user-info/index.js";
-export * from "./scx-user-info/ScxUserInfoVueInstaller.js";
-export * from "./scx-req/ScxReqVueInstaller.js";
 export * from "./scx-clustered-event-bus/ScxClusteredEventBusVueInstaller.js";
-export * from "./scx-fss/ScxFSSVueInstaller.js";
+export * from "./scx-config-manager/index.js";
 export * from "./scx-context-menu/index.js";
 export * from "./scx-drag/index.js";
+export * from "./scx-fetch/ScxFetchVueInstaller.js";
+export * from "./scx-fss/ScxFSSVueInstaller.js";
+export * from "./scx-perm/index.js";
+export * from "./scx-req/ScxReqVueInstaller.js";
+export * from "./scx-user-info/index.js";
+export * from "./scx-user-info/ScxUserInfoVueInstaller.js";
+export * from "./scx-component.js";
 
