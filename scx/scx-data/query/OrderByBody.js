df73869d65af8d089d0e93e406a4d349b0369791
scx567888 <scx567888@outlook.com>
2024-08-26 17:51:02 +0800

    dev

---
+++ scx/scx-data/query/OrderByBody.js
@@ -0,0 +1,41 @@
+import {isBlank} from "../../../util/index.js";
+import {QueryImpl} from "./QueryImpl.js";
+
+class OrderByBody {
+
+    #name;
+    #orderByType;
+    #info;
+
+    constructor(name, orderByType, info) {
+        if (isBlank(name)) {
+            throw new Error("OrderBy 参数错误 : 名称 不能为空 !!!");
+        }
+        if (orderByType == null) {
+            throw new Error("OrderBy 参数错误 : orderByType 不能为空 !!!");
+        }
+        this.#name = name.trim();
+        this.#orderByType = orderByType;
+        this.#info = info;
+    }
+
+
+    name() {
+        return this.#name;
+    }
+
+    orderByType() {
+        return this.#orderByType;
+    }
+
+    info() {
+        return this.#info;
+    }
+
+    toQuery() {
+        return new QueryImpl().orderBy(this);
+    }
+    
+}
+
+export {OrderByBody};
