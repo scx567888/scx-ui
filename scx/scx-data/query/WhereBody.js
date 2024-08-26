e6a4de6dece424ff4ec70a5797767e339cde58a9
scx567888 <scx567888@outlook.com>
2024-08-26 17:54:31 +0800

    dev

---
+++ scx/scx-data/query/WhereBody.js
@@ -0,0 +1,76 @@
+import {QueryLike} from "./QueryLike.js";
+import {isBlank} from "../../../util/index.js";
+import {QueryImpl} from "./QueryImpl.js";
+
+class WhereBody extends QueryLike {
+
+    #name;
+    #whereType;
+    #value1;
+    #value2;
+    #info;
+
+    constructor(name, whereType, value1, value2, info) {
+        super();
+        //名称不能为空
+        if (isBlank(name)) {
+            throw new Error("Where 参数错误 : 名称 不能为空 !!!");
+        }
+        //类型也不能为空
+        if (whereType == null) {
+            throw new Error("Where 参数错误 : whereType 不能为空 !!!");
+        }
+        //校验参数 并获取有效的参数数量(不为空的) 每检测到一个有效的(不为空的) 便加 1
+        let validParamSize = WhereBody.getValidParamSize(value1, value2);
+        //类型所需的参数数量和所传的合法参数数量必须一致
+        if (whereType.paramSize() !== validParamSize) {
+            throw new Error(whereType);
+        }
+        this.#name = name.trim();
+        this.#whereType = whereType;
+        this.#value1 = value1;
+        this.#value2 = value2;
+        this.#info = info;
+    }
+
+    static getValidParamSize(value1, value2) {
+        //有效的参数数量(不为空的) 每检测到一个有效的(不为空的) 便加 1
+        let validParamSize = 0;
+        if (value1 != null) {
+            validParamSize = validParamSize + 1;
+        }
+        if (value2 != null) {
+            validParamSize = validParamSize + 1;
+        }
+        return validParamSize;
+    }
+
+    name() {
+        return this.#name;
+    }
+
+    whereType() {
+        return this.#whereType;
+    }
+
+    value1() {
+        return this.#value1;
+    }
+
+    value2() {
+        return this.#value2;
+    }
+
+    info() {
+        return this.#info;
+    }
+
+    toQuery() {
+        return new QueryImpl().where([this]);
+    }
+
+}
+
+export {
+    WhereBody,
+};
