0c4876c78ed251b03c8c0281510b0de67efbc5db
scx567888 <scx567888@outlook.com>
2024-08-28 17:11:19 +0800

    dev

---
+++ scx/scx-data/field_filter/FilterMode.js
@@ -0,0 +1,19 @@
+class FilterMode {
+    #value;
+
+    constructor(value) {
+        this.#value = value;
+    }
+    
+    value(){
+        return this.#value;
+    }
+    
+}
+
+
+const INCLUDED=new FilterMode("INCLUDED");
+
+const EXCLUDED=new FilterMode("EXCLUDED");
+
+export { FilterMode,INCLUDED,EXCLUDED };
