51c95c6d3c176d2e1489033c20bef350ae481f36
scx567888 <scx567888@outlook.com>
2024-08-28 08:04:15 +0800

    dev

---
+++ scx/scx-data/query/Query.js
@@ -0,0 +1,45 @@
+class Query {
+    
+    where(whereClauses){}
+
+    groupBy(groupByClauses){}
+
+    orderBy(orderByClauses){}
+
+    addWhere(whereClauses){}
+
+    addGroupBy(groupByClauses){}
+
+    addOrderBy(orderByClauses){}
+
+    offset( limitOffset){}
+
+    limit(numberOfRows){}
+
+    getWhere(){}
+
+    getGroupBy(){}
+
+    getOrderBy(){}
+
+    getOffset(){}
+
+    getLimit(){}
+
+    getLimitInfo(){}
+
+    clearWhere(){}
+
+    clearGroupBy(){}
+
+    clearOrderBy(){}
+
+    clearOffset(){}
+
+    clearLimit(){}
+    
+}
+
+export {
+    Query
+}
