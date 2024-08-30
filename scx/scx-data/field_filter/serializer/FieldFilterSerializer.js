5fa3623dab25906e9a17b17ea8daa4ce80ab5828
scx567888 <scx567888@outlook.com>
2024-08-30 17:41:38 +0800

    dev

---
+++ scx/scx-data/field_filter/serializer/FieldFilterSerializer.js
@@ -0,0 +1,27 @@
+import {FieldFilter} from "../../FieldFilter.js";
+
+class FieldFilterSerializer {
+
+    serialize(obj) {
+        if (obj instanceof FieldFilter) {
+            return this.serializeFieldFilter(obj);
+        }
+        return obj;
+    }
+
+    serializeFieldFilter(fieldFilter) {
+        return {
+            "@type": "FieldFilter",
+            "filterMode": fieldFilter.getFilterMode(),
+            "fieldNames": fieldFilter.getFieldNames(),
+            "ignoreNullValue": fieldFilter.getIgnoreNullValue(),
+        };
+    }
+    
+}
+
+const FIELD_FILTER_SERIALIZER = new FieldFilterSerializer();
+
+export {
+    FieldFilterSerializer,
+};
